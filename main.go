5660418f1d68a8ca5331f39b36d713d635c35249
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-04-01 11:32:44 +0100

    functions

--- main.go
+++ main.go
@@ -150,9 +150,80 @@ func main() {
 
 
 //var x map[string]int;
-x:= make(map[string]int)
-x["key"] = 10
-x["world"] = 11
-fmt.Println(x["key"])
+// x:= make(map[string]int)
+// x["key"] = 10
+// x["world"] = 11
 
+// y:= make(map[int]int);
+// y[1]=100;
+
+// fmt.Println(y);
+
+
+// delete(x, "key");
+// fmt.Println(x)
+
+// elements := make(map[string]string)
+// elements["H"] = "Hydrogen"
+// elements["He"] = "Helium"
+// elements["Li"] = "Lithium"
+// elements["Be"] = "Beryllium"
+// elements["B"] = "Boron"
+// elements["C"] = "Carbon"
+// elements["N"] = "Nitrogen"
+// elements["O"] = "Oxygen"
+// elements["F"] = "Fluorine"
+// elements["Ne"] = "Neon"
+
+
+
+// name, ok := elements["H"]
+// fmt.Println(name, ok)
+
+
+elements := map[string]map[string]string{
+    "H": map[string]string{
+    "name":"Hydrogen",
+    "state":"gas",
+    },
+    "He": map[string]string{
+    "name":"Helium",
+    "state":"gas",
+    },
+    "Li": map[string]string{
+    "name":"Lithium",
+    "state":"solid",
+    },"Be": map[string]string{
+"name":"Beryllium",
+"state":"solid",
+},
+"B": map[string]string{
+"name":"Boron",
+"state":"solid",
+},
+"C": map[string]string{
+"name":"Carbon",
+"state":"solid",
+},
+"N": map[string]string{
+"name":"Nitrogen",
+"state":"gas",
+},
+"O": map[string]string{
+"name":"Oxygen",
+"state":"gas",
+},
+"F": map[string]string{
+"name":"Fluorine",
+"state":"gas",
+},
+"Ne": map[string]string{
+"name":"Neon",
+"state":"gas",
+},
 }
+if el, ok := elements["Ne"]; ok {
+fmt.Println(el["name"], el["state"]);
+}
+}
+
