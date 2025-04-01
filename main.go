07bc1af5204c557935ea9a58efac828c89ebe3d1
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-04-01 10:20:36 +0100

    mapping

--- main.go
+++ main.go
@@ -114,18 +114,35 @@ func main() {
 // is allowed to change
 
  
+//normal method
+// x := make([]float64,5,10);
 
-x := make([]float64,5,10);
-
-x[0] = 98
-x[1] = 93
-x[2] = 77
-x[3] = 82
-x[4] = 83
+// x[0] = 98
+// x[1] = 93
+// x[2] = 77
+// x[3] = 82z
+// x[4] = 83
 
 
-fmt.Println(x)
+// fmt.Println(x)
 
+
+// arr := [5]float64{1,2,3,4,5}
+// x := arr[0:5]
+
+// fmt.Println(x);
+
+//append 
+// slice1 := []int{1,2,3}
+// slice2 := append(slice1, 4, 5)
+// fmt.Println(slice1, slice2)
+
+//Copy
+
+slice1 := []int{1,2,3,6,8,9}
+slice2 := make([]int, 8)
+copy(slice2, slice1)
+fmt.Println(slice1, slice2)
 
 
 
