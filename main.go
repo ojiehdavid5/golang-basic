fa3517ae1f4dbf7e895fe9a30d9868cb48ec22e8
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-04-06 12:55:13 +0100

    package

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
     "time"
     // "math/rand"
     )
+    import m  "go/src/hello/math/math.go"
 
 // import "os"
 
@@ -363,39 +364,43 @@ func main() {
 
 
 //SELECT
-    c1 := make(chan string)
-    c2 := make(chan string)
-    go func() {
-    for {
-    c1 <- "from 1"
-    time.Sleep(time.Second * 0)
-    }
-    }()
-    go func() {
-    for {
-    c2 <- "from 2"
-    time.Sleep(time.Second * 0)
-    }
-    }()
-    go func() {
-    for {
-        select {
-        case msg1 := <- c1:
-        fmt.Println("Message 1", msg1)
-        case msg2 := <- c2:
-        fmt.Println("Message 2", msg2)
-        case <- time.After(time.Second):
-        fmt.Println("timeout")
+    // c1 := make(chan string)
+    // c2 := make(chan string)
+    // go func() {
+    // for {
+    // c1 <- "from 1"
+    // time.Sleep(time.Second * 0)
+    // }
+    // }()
+    // go func() {
+    // for {
+    // c2 <- "from 2"
+    // time.Sleep(time.Second * 0)
+    // }
+    // }()
+    // go func() {
+    // for {
+    //     select {
+    //     case msg1 := <- c1:
+    //     fmt.Println("Message 1", msg1)
+    //     case msg2 := <- c2:
+    //     fmt.Println("Message 2", msg2)
+    //     case <- time.After(time.Second):
+    //     fmt.Println("timeout")
 
-    default:
-        fmt.Println("nothing ready")
+    // default:
+    //     fmt.Println("nothing ready")
         
-        }
+    //     }
     
-    }
-    }()
-    var input string
-    fmt.Scanln(&input)
+    // }
+    // }()
+    // var input string
+    // fmt.Scanln(&input)
+
+        xs := []float64{1,2,3,4}
+        avg := m.Average(xs)
+        fmt.Println(avg)
 
 
 
