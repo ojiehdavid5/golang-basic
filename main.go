27d00d5eb32af04d42835fcea68e76cef9831950
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-04-07 17:54:30 +0100

    strings

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 // import "fmt";
 import (
     "fmt"
-    "time"
+    // "time"
     // "math/rand"
+    "strings"
     )
 
 // import "os"
@@ -414,9 +415,32 @@ func main() {
 
         //strings
 
-        
+        fmt.Println(
+            // true
+            strings.Contains("test", "es"),
+            // 2
+            strings.Count("test", "t"),
+            // true
+            strings.HasPrefix("test", "te"),
+            // true
+            strings.HasSuffix("test", "st"),
+            // 1
+            strings.Index("test", "e"),
+            // "a-b"
+            strings.Join([]string{"a","b"}, "-"),
+            // == "aaaaa"
+            strings.Repeat("a", 5),
+            // "bbaa"
+            strings.Replace("aaaa", "a", "b", 2),
+            // []string{"a","b","c","d","e"}
+            strings.Split("a-b-c-d-e", "-"),
+            // "test"
+        strings.ToLower("TEST"),
+// "TEST"
+        strings.ToUpper("test"),
+)
 
-        
+    
 
 
 
