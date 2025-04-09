87f1d4819761a2fada0aeb5eb2416a33a070d859
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-04-09 02:35:13 +0100

    hashes

--- main.go
+++ main.go
@@ -10,8 +10,9 @@ import (
     // "os"
     // "io/ioutil"
     // "path/filepath"
-    "errors"
-    )
+    // "errors"
+// "sort"
+)
 
 // import "os"
 
@@ -554,7 +555,12 @@ func main() {
 
 //container and sort
 //sort
-
+// kids := []Person{
+//     {"Jill",9},
+//     {"Jack",10},
+//     }
+//     sort.Sort(ByName(kids))
+//     fmt.Println(kids)
 
 
 }
@@ -768,18 +774,20 @@ func main() {
     //sort
 
 
-    type Person struct {
-        Name string
-        Age int
-        }
-        type ByName []Person
-        func (this ByName) Len() int {
-        return len(this)
-        }
-        func (this ByName) Less(i, j int) bool {
-        return this[i].Name < this[j].Name
-        }
-        func (this ByName) Swap(i, j int) {
-        this[i], this[j] = this[j], this[i]
-        }
+    // type Person struct {
+    //     Name string
+    //     Age int
+    //     }
+    //     type ByName []Person
+    //     func (this ByName) Len() int {
+    //     return len(this)
+    //     }
+    //     func (this ByName) Less(i, j int) bool {
+    //     return this[i].Name < this[j].Name
+    //     }
+    //     func (this ByName) Swap(i, j int) {
+    //     this[i], this[j] = this[j], this[i]
+    //     }
 
+
+    //HAshes
